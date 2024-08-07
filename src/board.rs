3e864d697030d59b3e92a3d0606141a2cd68aee6
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2024-08-07 22:44:27 +0900

    Add Board and Player

---
+++ src/board.rs
@@ -0,0 +1,84 @@
+use crate::player::Player;
+
+const NUM_POCKETS: usize = 6;
+const INITIAL_STONE_COUNT: i32 = 4;
+
+pub struct Board {
+    data: [i32; ((NUM_POCKETS + 1) * 2) as usize],
+}
+
+pub enum Position {
+    Store { player: Player },
+    Pocket { player: Player, idx: usize },
+}
+
+impl Board {
+    pub fn new() -> Self {
+        let mut data = [0; (NUM_POCKETS + 1) * 2];
+        for i in 0..NUM_POCKETS {
+            data[i] = INITIAL_STONE_COUNT;
+            data[NUM_POCKETS + 1 + i] = INITIAL_STONE_COUNT;
+        }
+
+        Board { data }
+    }
+
+    fn position_to_index(pos: &Position) -> usize {
+        match pos {
+            Position::Pocket {
+                player: Player::A,
+                idx,
+            } => *idx,
+            Position::Store { player: Player::A } => NUM_POCKETS,
+            Position::Pocket {
+                player: Player::B,
+                idx,
+            } => NUM_POCKETS + 1 + idx,
+            Position::Store { player: Player::B } => 2 * NUM_POCKETS + 1,
+        }
+    }
+
+    pub fn count(&self, pos: &Position) -> i32 {
+        self.data[Self::position_to_index(pos)]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn count_initial() {
+        let board = Board::new();
+        assert_eq!(
+            4,
+            board.count(&Position::Pocket {
+                player: Player::A,
+                idx: 0
+            })
+        );
+        assert_eq!(
+            4,
+            board.count(&Position::Pocket {
+                player: Player::A,
+                idx: 5
+            })
+        );
+        assert_eq!(0, board.count(&Position::Store { player: Player::A }));
+        assert_eq!(
+            4,
+            board.count(&Position::Pocket {
+                player: Player::B,
+                idx: 0
+            })
+        );
+        assert_eq!(
+            4,
+            board.count(&Position::Pocket {
+                player: Player::B,
+                idx: 5
+            })
+        );
+        assert_eq!(0, board.count(&Position::Store { player: Player::B }));
+    }
+}
---
+++ src/player.rs
@@ -0,0 +1,4 @@
+pub enum Player {
+    A,
+    B,
+}
