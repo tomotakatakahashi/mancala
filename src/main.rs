ef7554ea95172f46c79480dee17c4f9b9acfb059
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2024-08-10 10:02:36 +0900

    Improve the main function

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,45 @@ use mancala::game::{select, Turn};
 use mancala::player::Player;
 use std::io;
 
-fn main() {
-    let board = Board::new();
-    let turn = Turn::InProgress { next: Player::A };
-    cliio::print(&board);
-
+fn get_input() -> usize {
     let mut cmd = String::new();
     io::stdin()
         .read_line(&mut cmd)
         .expect("Failed to read line");
     let cmd_int: usize = cmd.trim().parse::<usize>().expect("Please type a number!") - 1;
     if !(0..NUM_POCKETS).contains(&cmd_int) {
+        // TODO: Retry
         println!("Please type a number between 1 and {NUM_POCKETS}");
         panic!()
     }
+    cmd_int
+}
 
-    let (turn, board) = select(
-        &board,
-        &Position::Pocket {
-            // TODO: Use correct player.
-            player: Player::A,
-            idx: cmd_int,
-        },
-    );
+fn main() {
+    let mut board = Board::new();
+    let mut turn = Turn::InProgress { next: Player::A };
 
-    cliio::print(&board);
+    loop {
+        cliio::print(&board);
+        match turn {
+            Turn::InProgress { next } => {
+                let cmd = get_input();
+                (turn, board) = select(
+                    &board,
+                    &Position::Pocket {
+                        player: next,
+                        idx: cmd,
+                    },
+                );
+            }
+            Turn::Finished { winner } => {
+                let player_name = match winner {
+                    Player::A => "First player",
+                    Player::B => "Second player",
+                };
+                println!("{player_name} won!");
+                break;
+            }
+        }
+    }
 }
